e0085575ae6e7bb679481652cd6ef0bf10587bf9
huytran <thtran.082@gmail.com>
2022-07-15 10:38:48 +0700

    refactor: update UI for the menu in navbar

--- src/app/shared/ui/components/navbar/navbar.component.ts
+++ src/app/shared/ui/components/navbar/navbar.component.ts
@@ -1,9 +1,9 @@
-import { CdkMenuModule, CdkMenuTrigger } from '@angular/cdk/menu';
-import { CommonModule } from '@angular/common';
-import { Component, ContentChildren, QueryList } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { AuthStore } from '../../../data-access';
-import { NavbarItemDirective } from './navbar-item.directive';
+import { CdkMenuModule } from "@angular/cdk/menu";
+import { CommonModule } from "@angular/common";
+import { Component, ContentChildren, QueryList } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { AuthStore } from "../../../data-access";
+import { NavbarItemDirective } from "./navbar-item.directive";
 
 const ANGULAR_MODULES = [CommonModule, RouterModule];
 const MAT_MODULES = [CdkMenuModule];
@@ -18,22 +18,8 @@ const DIRECTIVES = [NavbarItemDirective];
 export class NavbarComponent {
   @ContentChildren(NavbarItemDirective) items!: QueryList<NavbarItemDirective>;
   readonly auth$ = this._authStore.auth$;
-  isMenuOpen = false;
 
   constructor(private _authStore: AuthStore) {}
-
-  openMenu(menuTrigger: CdkMenuTrigger): void {
-    if (!this.isMenuOpen) {
-      menuTrigger.open();
-    }
-    this.isMenuOpen = true;
-  }
-
-  closeMenu(menuTrigger: CdkMenuTrigger): void {
-    if (!this.isMenuOpen) {
-      menuTrigger.close();
-    }
-  }
 
   logout(): void {
     this._authStore.logout();
